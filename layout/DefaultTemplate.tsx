ed21527171b52d42c1dcf99cfaeca45516b60c72
Kalyan Annamalai <kalyan.annamalai@agriculture.gov.au>
2021-08-01 21:01:36 +1000

    material ui config

---
+++ layout/DefaultTemplate.tsx
@@ -0,0 +1,27 @@
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  mainContent: {
+    flexGrow: 2,
+  },
+});
+
+export const DefaultTemplate: React.FC = ({ children }) => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.root}>
+      <Header />
+      <main className={styles.mainContent}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+
+import type { AppProps } from "next/app";
+import { DefaultTemplate } from "../layout";
+import Head from "next/head";
+import theme from "../styles/theme";
+import { useEffect } from "react";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>BizExpo</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <DefaultTemplate>
+          <Component {...pageProps} />
+        </DefaultTemplate>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;

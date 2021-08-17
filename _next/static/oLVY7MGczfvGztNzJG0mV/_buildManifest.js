4c7b640d420fe5f595f8ce7f4441dff4f769211a
Kalyan Annamalai <kalyan.shelf@gmail.com>
2021-08-18 02:15:13 +1000

    Updates

--- _next/static/oLVY7MGczfvGztNzJG0mV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b941917f8afa6512d2e1.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
